33021f6e90081404aea360c24c8c91af77c75ab5
andy <andy@sammalmaa.fi>
2023-02-16 23:45:12 +0200

    examples: expand basic usage to show `task` command

--- example/basic/build.sh
+++ example/basic/build.sh
@@ -15,19 +15,27 @@ trace group finish "${clone}"
 docker=$(trace group start "docker")
 
   pulls=$(trace group start "pull" "${docker}")
-    # docker pull
-    sleep 2s
+
+    trace task "$pulls" -- docker pull alpine:latest
+
   trace group finish "${pulls}"
 
   build=$(trace group start "build" "${docker}")
-    # docker builds
-    sleep 2s
+
+    trace task "$build" -- docker build . --no-cache
+    trace task "$build" -- sleep 5s
+
   trace group finish "${build}"
 
 
   push=$(trace group start "push" "${docker}")
-    # docker push
-    sleep 2s
+
+    trace task "$push" --  sleep 2s &
+    trace task "$push" --  sleep 10s &
+    trace task "$push" --  sleep 5s &
+
+    wait
+
   trace group finish "${push}"
 
 trace group finish "${docker}"
